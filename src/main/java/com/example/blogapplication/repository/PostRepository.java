48df23e74b72f0d5f5d17fdd1f152cc4eb417a1e
GiaPhat <phatng.it.dev@gmail.com>
2023-08-14 19:00:32 +0700

    [add] JPA Respository - CommentRepository

--- src/main/java/com/example/blogapplication/repository/PostRepository.java
+++ src/main/java/com/example/blogapplication/repository/PostRepository.java
@@ -16,4 +16,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<Post,Long> {
 }
 
-// public interface CommentRepository extends JpaRepository<Comment,Long>{}
