f4347233e521e35450849b5c9258bd4dc62efe7d
GiaPhat <phatng.it.dev@gmail.com>
2023-08-16 02:00:39 +0700

    [refactor] Get All Posts with comments that belong to the post

--- src/main/java/com/example/blogapplication/payload/PostDto.java
+++ src/main/java/com/example/blogapplication/payload/PostDto.java
@@ -2,12 +2,15 @@ package com.example.blogapplication.payload;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDto  {
     private long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 
     // Định nghĩa lớp DTO: DTO chứa các trường dữ liệu mà bạn muốn truyền giữa khác tầng.
     // DTO class không nên chưa logic business, chỉ nên chưa các filed và getter/setter
