44bdca55bb7bfcd20ba986f1f1e8f321d71090ef
GiaPhat <phatng.it.dev@gmail.com>
2023-08-17 13:14:18 +0700

    [add] Validate and Customizing validation Response

--- src/main/java/com/example/blogapplication/controller/PostController.java
+++ src/main/java/com/example/blogapplication/controller/PostController.java
@@ -4,6 +4,7 @@ import com.example.blogapplication.payload.PostDto;
 import com.example.blogapplication.payload.PostResponse;
 import com.example.blogapplication.service.PostService;
 import com.example.blogapplication.utils.AppConstants;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,7 @@ public class PostController {
 
     // create blog post
     @PostMapping()
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
 
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
@@ -45,7 +46,7 @@ public class PostController {
 
     // update post by id api
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto,@PathVariable(name = "id") long id){
+    public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto,@PathVariable(name = "id") long id){
         PostDto postResponse = postService.updatePost(postDto,id);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
